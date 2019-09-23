637397ab94748877db9fa38f298556cfe34b0d31
Damian Hijnen <damianhijnen@hotmail.com>
2019-09-23 12:01:23 +0200

    start game

---
+++ game.js
@@ -0,0 +1,47 @@
+const requirement = alert ("welkome bij de game");
+const mesage1 = alert ("je moet alle vragen met A of B beantwoorden");	
+const mmesage1 = alert ("begin van de game");
+const  mesage2 = alert("je word wakker en lig lekker in bed");
+const mesage3 =("je bent een beetje moe maar je moet uit bed");
+var bed = prompt("A stap je uit bed / B blijf liggen ")
+
+	if (bed == "A"){
+		const mesage1a = alert("je gaat naar beneden")
+		const mesage2a = alert("wat voor ondbijt neem je deze ochtend")
+		var masage3a = prompt("A salade B pizza")
+		if (masage3a == "A") {
+			document.write("je stikte in een fucking blaadje sla")}
+	}
+		if (masage3a == "B"){
+		const mesage3a1 = alert("zul hem in de oven of de magnatron doen")
+		var mesage3a2 = prompt("A oven B magnatron")
+
+		if (mesage3a2 == "A") {
+			const mesage3a2a1 = alert("Na een halfuurtje wachten heb je een heerlijke pizza op")
+
+		}
+
+		if (mesage3a2 == "B") { document.write("je kreeg kanker and you fucking died")
+
+		}
+		}
+
+	if (bed == "B"){
+		const mesage1b = alert("ow oke dat kan ook blijf maar lekker in bed liggen")
+		const mesage2b =  alert("Je moeder komt boos naar boven en zegt dat je naar school moet")
+		var mesage3b = prompt("a stap uit bed / b ben ziek ")
+		}
+		if (mesage3b == "A") {
+
+		}
+
+
+
+		if (mesage3b == "B") {
+			const mesage3b1b = alert("m: ok")
+			document.write("Sleepy ending")
+		
+			}
+
+
+
